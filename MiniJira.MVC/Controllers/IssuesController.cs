8ab10dc011fa43a54ec316bcdfe606940100ba1b
Sanjay Chaudhary <sanjay.chaudhary.mbs@gmail.com>
2025-11-19 19:11:25 +0530

    Added Homne page,Privacy and Policy Issue create also

---
+++ MiniJira.MVC/Controllers/IssuesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniJira.MVC.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MiniJira.MVC.Controllers
+{
+    public class IssuesController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public IssuesController(IHttpClientFactory factory)
+        {
+            _client = factory.CreateClient("MiniJiraAPI");
+        }
+
+        // GET: Issues/Create
+        public IActionResult Create(int projectId)
+        {
+            var model = new IssueViewModel { ProjectId = projectId };
+            return View(model);
+        }
+
+        // POST: Issues/Create
+        [HttpPost]
+        public async Task<IActionResult> Create(IssueViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var json = JsonConvert.SerializeObject(model);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync("api/issues", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Redirect back to Project Details page
+                return RedirectToAction("Details", "Projects", new { id = model.ProjectId });
+            }
+
+            ViewBag.Error = "Failed to create issue.";
+            return View(model);
+        }
+    }
+}
